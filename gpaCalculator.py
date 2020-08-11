bf07b6a7caf9dc1c31d8f25f2f30b10c44bdff22
silverdoe23 <artsgirl.sofia@gmail.com>
2020-08-10 21:13:40 -0400

    This is my first github commit.

---
+++ gpaCalculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+
+
+#HT of letter grades (A+, A, A-, B+, B, B-, etc.) and numbers (4, 4, 3.7, )
+
+
+#main user interaction function
+#function that translates grade to number
+#function that adds credits
+
+
+#user passes in user's .txt file
+#prints the GPA, then the steps if desired
