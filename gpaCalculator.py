5e2abb53a36478586219019dde9cf4cdd428959d
silverdoe23 <artsgirl.sofia@gmail.com>
2020-08-10 21:19:57 -0400

    We changed a line.

--- gpaCalculator.py
+++ gpaCalculator.py
@@ -4,7 +4,6 @@
 
 #HT of letter grades (A+, A, A-, B+, B, B-, etc.) and numbers (4, 4, 3.7, )
 
-
 #main user interaction function
 #function that translates grade to number
 #function that adds credits
@@ -12,3 +11,4 @@
 
 #user passes in user's .txt file
 #prints the GPA, then the steps if desired
+#git is so awesome ;)
