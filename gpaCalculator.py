b0c70bdecd277b7688dcd380aafeaf096059f377
silverdoe23 <artsgirl.sofia@gmail.com>
2020-08-13 20:02:31 -0400

    Here's my version of the normal and weighted GPA system.

--- gpaCalculator.py
+++ gpaCalculator.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+gpaNumbersWeighted = {'A+': 4.5, 'A':4, 'A-':3.7, 'B+':3.3, 'B':3, 'B-':2.7, 'C+': 2.4, 'C':2, 'C-':1.7, 'F':1}
+gpaNumbers = {'A+':4, 'A':3.7, 'A-':3.3, 'B+':3, 'B':2.7, 'B-':2.3, 'C+':2, 'C':1.7, 'C-':1.3, 'F':1}
 
 
 #HT of letter grades (A+, A, A-, B+, B, B-, etc.) and numbers (4, 4, 3.7, )
