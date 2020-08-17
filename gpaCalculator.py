085ec6eb3a18e15a3680b774c144bdfe2012014d
silverdoe23 <artsgirl.sofia@gmail.com>
2020-08-17 19:54:13 -0400

    credit hashtables

--- gpaCalculator.py
+++ gpaCalculator.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
-gpaNumbersWeighted = {'A+': 4.5, 'A':4, 'A-':3.7, 'B+':3.3, 'B':3, 'B-':2.7, 'C+': 2.4, 'C':2, 'C-':1.7, 'F':1}
-gpaNumbers = {'A+':4, 'A':3.7, 'A-':3.3, 'B+':3, 'B':2.7, 'B-':2.3, 'C+':2, 'C':1.7, 'C-':1.3, 'F':1}
+gpaNumbers = {'A+': 4.0, 'A':4.0, 'A-':3.7, 'B+':3.3, 'B':3.0, 'B-':2.7, 'C+': 2.4, 'C':2.0, 'C-':1.7, 'F':1.0}
 
 
 #HT of letter grades (A+, A, A-, B+, B, B-, etc.) and numbers (4, 4, 3.7, )
